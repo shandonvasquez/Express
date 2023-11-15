a2d966b47edaff604dc32c94e3a6c1fc16c6fb47
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-11-15 08:51:07 -0600

    This has some changes to the index

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Mover el código del evento click al archivo script.js
 var button = document.getElementsByTagName('button')[0];
 
 button.addEventListener('click', function() {
     console.log('click!!!');
 });
 
-//importa esto en el index.html
 
+//importa esto en el index.html
+//
