df08be101fc11b76e0b5af850849e41d7c52e91e
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-11-16 22:45:41 -0600

    This has some changes to the index

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ button.addEventListener('click', function() {
     console.log('click!!!');
 });
 
+app.use(express.static('public', {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.js')) {
+        res.setHeader('Content-Type', 'text/javascript');
+      }
+    }
+  }));
+  
 
 //importa esto en el index.html
 //
